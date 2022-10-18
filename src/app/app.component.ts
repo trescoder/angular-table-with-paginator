fa5ef362e4148cd17cdac3bb680ccd4678c7fc94
Fernando Orozco <forozcomercado@gmail.com>
2022-10-18 16:53:03 -0500

    rxjs map method working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { ReqResResponse, ReqResService, User } from './req-res.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'common-rxjs-operators';
+  public usersList!: Observable<User[]>;
+
+  constructor(private reqResService: ReqResService){}
+
+  displayUserList(){
+    this.usersList = this.reqResService.getUserList().pipe(
+      map((value: ReqResResponse) => value.data)
+    )
+  }
 }
---
+++ src/app/req-res.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
+
+export interface ReqResResponse {
+  page:number;
+  per_page: number;
+  total: number;
+  data: User[]
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReqResService {
+  private readonly url = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  public getUserList(page:number = 2): Observable<ReqResResponse> {
+    return this.http.get<ReqResResponse>(`${this.url}users?page=${page}`);
+  }
+}
